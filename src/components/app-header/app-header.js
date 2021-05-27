97c49930d3055968931a2331cf94a21c7e9053ba
olexander-shvets <olexandershvets@gmail.com>
2021-05-27 21:15:31 +0300

    Initial commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Alex Shvets</h1>
+            <h2>5 posts, 2 liked</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
